4d88076f23b5533f629f1277f55b852d36963010
Maximiliano <72700320+maxtheprogrammer98@users.noreply.github.com>
2022-08-11 16:38:02 -0300

    Archivo .py - Ejercicio 6 del TP grupal.

---
+++ ejercicio_6_python.py
@@ -0,0 +1,23 @@
+#tupla base:
+historial2 = (23500, 5960, 2300, 10200, 8900
+)
+
+#funcion 1: calcula los gastos mayores a $5.000
+def gastos_mayores(elem):
+    contador = 0
+    for i in elem:
+        if i > 5000:
+            contador += 1
+    print(f"gastos mayores a $5.000: {contador}")
+    
+#funcion 2: calcula cuanto se gasto en total por todas las
+# consultas / servicios.
+def gastos_totales(elem):
+    contador = 0
+    for i in elem:
+        contador += i
+    print(f"gastos totales en atención a Frida: ${contador}")
+
+#ejecutando funciones:
+gastos_mayores(historial2)
+gastos_totales(historial2)
