505f5eee220a15771029dbb7e8c1eb7ebc267cb4
Soledad Quiroz <soledadquiroz96@gmail.com>
2022-08-14 14:33:11 -0300

    Correcciones en ejercicio sql

--- peluqueria_canina.sql
+++ peluqueria_canina.sql
@@ -1,26 +1,29 @@
 CREATE DATABASE peluqueria_canina;
 use peluqueria_canina;
+-- Tabla Dueno
 CREATE TABLE Dueno (
     DNI INT NOT NULL,
     Nombre VARCHAR(50) NOT NULL,
     Apellido VARCHAR(50) NOT NULL,
-    Telefono INT NOT NULL,
+    Telefono BIGINT NOT NULL,
     Direccion VARCHAR(100) NOT NULL,
     PRIMARY KEY (DNI)
 );
+-- Tabla Perro
 CREATE TABLE Perro (
     ID_Perro INT NOT NULL AUTO_INCREMENT,
     Nombre_perro VARCHAR(50) NOT NULL,
-    Fecha_nac VARCHAR(50) NOT NULL,
-    Sexo VARCHAR(30) NOT NULL,
+    Fecha_nac DATE NOT NULL,
+    Sexo VARCHAR(10) NOT NULL,
     DNI_Dueno INT NOT NULL,
     PRIMARY KEY (ID_Perro),
     FOREIGN KEY (DNI_Dueno)
         REFERENCES Dueno (DNI)
 );
+-- Tabla Historial
 CREATE TABLE Historial (
     ID_Historial INT NOT NULL AUTO_INCREMENT,
-    Fecha VARCHAR(50) NOT NULL,
+    Fecha DATE NOT NULL,
     Perro INT NOT NULL,
     Descripcion VARCHAR(100) NOT NULL,
     Monto INT NOT NULL,
@@ -28,10 +31,10 @@ CREATE TABLE Historial (
     FOREIGN KEY (perro)
         REFERENCES Perro (ID_Perro)
 );
-#Inserte en la tabla correspondiente un nuevo animal (perro) como paciente y el dueño asociado a ese animal.
-INSERT INTO Dueno(DNI, Nombre, Apellido, Telefono, Direccion) VALUES (33574932, "Marina", "Lopez", "1186754569", "Colombia 455");
-INSERT INTO Perro(Nombre_perro, Fecha_nac, Sexo, DNI_dueno) VALUES ("Coca", "5/3/2018", "Hembra", 33574932);
-#Escriba una consulta que permita actualizar la dirección de un dueño. Su nueva dirección es Libertad 123
+-- Inserte en la tabla correspondiente un nuevo animal (perro) como paciente y el dueño asociado a ese animal.
+INSERT INTO Dueno VALUES (33574932, "Marina", "Lopez", "1186754569", "Colombia 455");
+INSERT INTO Perro(Nombre_perro, Fecha_nac, Sexo, DNI_dueno) VALUES ("Coca", "2018-03-05", "Hembra", 33574932);
+-- Escriba una consulta que permita actualizar la dirección de un dueño. Su nueva dirección es Libertad 123
 UPDATE Dueno 
 SET 
     Direccion = 'Libertad 123'
